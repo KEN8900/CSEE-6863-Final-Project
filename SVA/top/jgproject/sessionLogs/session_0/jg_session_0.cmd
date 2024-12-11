8b6aaf2d3b141e9287261be6517f602bd24b8194
ym3000 <ym3000@micro27.ee.columbia.edu>
2024-12-11 13:22:50 -0500

    nonseq pslex fixed

--- SVA/top/jgproject/sessionLogs/session_0/jg_session_0.cmd
+++ SVA/top/jgproject/sessionLogs/session_0/jg_session_0.cmd
@@ -5,10 +5,10 @@
 # version   : 2024.06p002 64 bits
 # build date: 2024.09.02 16:28:38 UTC
 # ----------------------------------------
-# started   : 2024-12-09 20:04:40 EST
-# hostname  : micro11.(none)
-# pid       : 1859264
-# arguments : '-label' 'session_0' '-console' '//127.0.0.1:33305' '-style' 'windows' '-data' 'AAAAfnicY2RgYLCp////PwMYMD6A0Aw2jAyoAMRnQhUJbEChGRhYYZqRNYkxlDDkMxQwxDMUM5QxJDLoAfnJDDlgeQDxagtE' '-proj' '/homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/jgproject/sessionLogs/session_0' '-init' '-hidden' '/homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/jgproject/.tmp/.initCmds.tcl' 'top_sva.tcl'
+# started   : 2024-12-11 13:05:39 EST
+# hostname  : micro27.(none)
+# pid       : 1345196
+# arguments : '-label' 'session_0' '-console' '//127.0.0.1:34087' '-style' 'windows' '-data' 'AAAAfnicY2RgYLCp////PwMYMD6A0Aw2jAyoAMRnQhUJbEChGRhYYZqRNYkxlDDkMxQwxDMUM5QxJDLoAfnJDDlgeQDxagtE' '-proj' '/homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/jgproject/sessionLogs/session_0' '-init' '-hidden' '/homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/jgproject/.tmp/.initCmds.tcl' 'top_sva.tcl'
 clear -all
 
 analyze -sv09 top_sva.sv
@@ -20,51 +20,6 @@ elaborate -top Bridge_Top
 clock clk
 reset ~rst
 prove -bg -all
-visualize -violation -property <embedded>::Bridge_Top.chk_top.assert_same_HPwrite_read -new_window
-visualize -violation -property <embedded>::Bridge_Top.chk_top.assert_read_addr0 -new_window
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-prove -bg -all
-visualize -violation -property <embedded>::Bridge_Top.chk_top.assert_nonseq_write -new_window
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-prove -bg -all
-visualize -violation -property <embedded>::Bridge_Top.chk_top.assert_nonseq_write -new_window
-visualize -violation -property <embedded>::Bridge_Top.chk_top.assert_nonseq_read -new_window
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-prove -bg -all
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-prove -bg -all
-prove -bg -all
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-prove -bg -all
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-prove -bg -all
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-prove -bg -all
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-prove -bg -all
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-prove -bg -all
-visualize -violation -property <embedded>::Bridge_Top.chk_top.assert_nonseq_psel -new_window
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-prove -bg -all
-visualize -violation -property <embedded>::Bridge_Top.chk_top.assert_nonseq_pselx -new_window
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-prove -bg -all
-visualize -violation -property <embedded>::Bridge_Top.chk_top.assert_nonseq_pselx -new_window
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-prove -bg -all
-visualize -violation -property <embedded>::Bridge_Top.chk_top.assert_nonseq_pselx -new_window
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-prove -bg -all
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-prove -bg -all
 visualize -violation -property <embedded>::Bridge_Top.chk_top.assert_nonseq_pselx -new_window
 include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
 prove -bg -all
@@ -73,6 +28,3 @@ include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_
 prove -bg -all
 include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
 prove -bg -all
-include /homes/user/stud/fall23/ym3000/6863/CSEE-6863-Final-Project/SVA/top/top_sva.tcl
-prove -bg -all
-visualize -violation -property <embedded>::Bridge_Top.chk_top.assert_nonseq_pselx -new_window
